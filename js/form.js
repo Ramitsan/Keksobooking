173557e80473d3786ba036bddb305b214ec7ae9d
Ramitsan <ramitsan@gmail.com>
2020-10-22 18:39:31 +0300

    Добавляет валидацию остальных полей формы

--- js/form.js
+++ js/form.js
@@ -24,30 +24,30 @@
   }
 
   // установка соответствия количества комнат и количества гостей
-  const roomsQuantity = adFormElement.querySelector(`#room_number`);
-  const guestsQuantity = adFormElement.querySelector(`#capacity`);
+  const roomsQuantityElement = adFormElement.querySelector(`#room_number`);
+  const guestsQuantityElement = adFormElement.querySelector(`#capacity`);
   const adFormSubmitButton = adFormElement.querySelector(`.ad-form__submit`);
 
-  const selectRooms = () => {
-    if (roomsQuantity.value === `100`) {
-      roomsQuantity.setCustomValidity(`100 комнат - только для королей!`);
-    } else if (roomsQuantity.value > guestsQuantity.value) {
-      roomsQuantity.setCustomValidity(`Комнат больше, чем гостей`);
-    } else if (roomsQuantity.value < guestsQuantity.value) {
-      roomsQuantity.setCustomValidity(`Комнат меньше, чем гостей`);
+  const selectRoomsHandler = () => {
+    if (roomsQuantityElement.value === `100`) {
+      roomsQuantityElement.setCustomValidity(`100 комнат - только для королей!`);
+    } else if (roomsQuantityElement.value > guestsQuantityElement.value) {
+      roomsQuantityElement.setCustomValidity(`Комнат больше, чем гостей`);
+    } else if (roomsQuantityElement.value < guestsQuantityElement.value) {
+      roomsQuantityElement.setCustomValidity(`Комнат меньше, чем гостей`);
     } else {
-      roomsQuantity.setCustomValidity(``);
+      roomsQuantityElement.setCustomValidity(``);
     }
   }
 
-  adFormSubmitButton.addEventListener(`click`, selectRooms);
+  adFormSubmitButton.addEventListener(`click`, selectRoomsHandler);
 
   // проверка заголовка объявления
   const titleInputElement = adFormElement.querySelector(`#title`);
   const MIN_TITLE_LENGTH = 30;
   const MAX_TITLE_LENGTH = 100;
 
-  titleInputElement.addEventListener(`input`, () => {
+  const checkTitleHandler = () => {
     const titleValueLength = titleInputElement.value.length;
     if (titleValueLength < MIN_TITLE_LENGTH) {
       titleInputElement.setCustomValidity(`Минимальная длина заголовка - ${MIN_TITLE_LENGTH} символов. Наберите еще ${MIN_TITLE_LENGTH - titleValueLength} символов`);
@@ -56,29 +56,46 @@
     } else {
       titleInputElement.setCustomValidity(``);
     }
-  })
+  }
+
+  titleInputElement.addEventListener(`input`, checkTitleHandler);
 
   // устанавливаем соответствие типа жилья и минимальной цены за ночь
   const typeInputElement = adFormElement.querySelector(`#type`);
   const priceInputElement = adFormElement.querySelector(`#price`);
 
-  const selectTypeAndPrice = () => {
-    if (typeInputElement.value === `bungalow`) {
-      priceInputElement.min === `0`;
-      priceInputElement.placeholder = `0`;
-    } else if (typeInputElement.value === `flat`) {
-      priceInputElement.min === `1000`;
-      priceInputElement.placeholder = `1 000`;
-    } else if (typeInputElement.value === `house`) {
-      priceInputElement.min === `5000`;
-      priceInputElement.placeholder = `5 000`;
-    } else if (typeInputElement.value === `palace`) {
-      priceInputElement.min === `10000`;
-      priceInputElement.placeholder = `10 000`;
+  const selectTypeAndPriceHandler = () => {
+    switch (typeInputElement.value) {
+      case `bungalow`:
+        priceInputElement.min === `0`;
+        priceInputElement.placeholder = `0`;
+        break;
+      case `flat`:
+        priceInputElement.min === `1000`;
+        priceInputElement.placeholder = `1 000`;
+        break;
+      case `house`:
+        priceInputElement.min === `5000`;
+        priceInputElement.placeholder = `5 000`;
+        break;
+      case `palace`:
+        priceInputElement.min === `10000`;
+        priceInputElement.placeholder = `10 000`;
+        break;
     }
   }
 
-  typeInputElement.addEventListener(`change`, selectTypeAndPrice);
+  typeInputElement.addEventListener(`change`, selectTypeAndPriceHandler);
+
+  // устанавливаем соответствие времени заезда и выезда
+  const timeInInputElement = adFormElement.querySelector(`#timein`);
+  const timeOutInputElement = adFormElement.querySelector(`#timeout`);
+
+  const selectTimeHandler = () => {
+    timeOutInputElement.value = timeInInputElement.value;
+  }
+
+  timeInInputElement.addEventListener(`change`, selectTimeHandler);
 
   window.form = {
     adFormElement: adFormElement,
